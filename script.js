dbbec339e5154fe7cbbe11f880ea6a65c506884e
cacorrales <85404903+cacorrales@users.noreply.github.com>
2023-04-15 19:17:22 -0500

    ca.corrales: Actualizacion Final Encriptador

---
+++ script.js
@@ -0,0 +1,62 @@
+function validar() {
+    var mensaje = document.getElementById("mensaje").value;
+    if (!/^[a-z]+$/.test(mensaje)) {
+      alert("Por favor, ingrese solo letras minúsculas.");
+      return false;
+    }
+    return true;
+  }
+
+function encriptar() {
+    if (!validar()) {
+      return;
+    }
+    var mensaje = document.getElementById("mensaje").value;
+    var resultado = "";
+    for (var i = 0; i < mensaje.length; i++) {
+      var letra = mensaje.charAt(i);
+      if (letra === 'a') {
+        resultado += 'ai';
+      } else if (letra === 'e') {
+        resultado += 'enter';
+      } else if (letra === 'i') {
+        resultado += 'ines';
+      } else if (letra === 'o') {
+        resultado += 'ober';
+      } else if (letra === 'u') {
+        resultado += 'ufat';
+      } else {
+        resultado += letra;
+      }
+    }
+    document.getElementById("resultado").textContent = resultado;
+  }
+
+  function copiar() {
+    var resultado = document.getElementById("resultado");
+    var copia = resultado.textContent;
+    navigator.clipboard.writeText(copia);
+    alert("¡El texto encriptado ha sido copiado al portapapeles!");
+  }
+  
+  function desencriptar() {
+    var resultado = document.getElementById("resultado").textContent;
+    var desencriptado = "";
+    for (var i = 0; i < resultado.length; i++) {
+      var letra = resultado.charAt(i);
+      if (letra === 'ai') {
+        desencriptado += 'a';
+      } else if (letra === 'enter') {
+        desencriptado += 'e';
+      } else if (letra === 'ines') {
+        desencriptado += 'i';
+      } else if (letra === 'ober') {
+        desencriptado += 'o';
+      } else if (letra === 'ufat') {
+        desencriptado += 'u';
+      } else {
+        desencriptado += letra;
+      }
+    }
+    document.getElementById("desencriptado").textContent = desencriptado;
+  }
